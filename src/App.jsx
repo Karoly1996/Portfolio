ad96caa2d846553a88945d23c44963851ee8572a
Karoly1996 <futyi1996@hotmail.com>
2024-08-21 13:53:44 -0400

    Finished About Me

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./Components/About/About"
 import { Hero } from "./Components/Hero/Hero"
 import Navbar from "./Components/navbar/Navbar"
+import Services from "./Components/Services/Services"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar></Navbar>
       <Hero></Hero>
       <About></About>
+      <Services></Services>
     </div>
   )
 }
--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -16,15 +16,33 @@ const About = () => {
         </div>
         <div className="about-right">
           <div className="about-para">
-            <p></p>
-            <p></p>
+            <p>Im an aspiring software developer eager to contribute my skills to real-world projects. I specialize in building clean, efficient web applications.</p>
+            <p>Based in Canada, Im seeking opportunities to apply my knowledge, grow as a developer, and make an impact in a dynamic team.</p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill"><p>HTML & CSS</p><hr style={{width: "50%"}}/></div>
+            <div className="about-skill"><p>React JS</p><hr style={{width: "70%"}}/></div>
+            <div className="about-skill"><p>JavaScript</p><hr style={{width: "60%"}}/></div>
+            <div className="about-skill"><p>Next JS</p><hr style={{width: "50%"}}/></div>
           </div>
         </div>
       </div>
-
-
-
-
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>10+</h1>
+          <p>YEARS OF EXPERIENCE</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>90+</h1>
+          <p>PROJECTS COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>15+</h1>
+          <p>HAPPY CLIENTS</p>
+        </div>
+      </div>
     </div>
   )
 }
